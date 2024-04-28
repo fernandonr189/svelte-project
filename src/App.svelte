067ff5473f9937587b598e64e8748ab9c5cd761e
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2024-04-28 16:13:20 -0600

    created primarybutton

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,16 @@
 <script>
-  import Counter from './lib/Counter.svelte'
+  import PrimaryButton from './lib/PrimaryButton.svelte'
+  import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+  
+  let counter = 0;
+
+  let increment = () => {
+    counter += 1
+  }
 </script>
 
 <main>
   <div class="bg-slate-800 flex text-white h-screen p-5 justify-center items-center">
-    <Counter />
+    <PrimaryButton func={increment} title="Count is {counter}"/>
   </div>
 </main>
---
+++ src/lib/PrimaryButton.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Fa from 'svelte-fa'
+  export let prefixIcon = undefined
+  export let sufixIcon = undefined
+  export let func
+  export let title
+
+</script>
+
+<button on:click={func} class="bg-slate-900 shadow-sm hover:shadow-lg border-solid border-2 p-2 border-slate-400 hover:border-cyan-600 hover:bg-cyan-950 rounded-lg">
+  <div class="flex items-center">
+    <Fa icon={prefixIcon} class="mx-1"/>
+    <p class="mx-1">
+      {title}
+    </p>
+    <Fa icon={sufixIcon} class="mx-1"/>
+  </div>
+</button>
