95d161b2c583e9cb8a319074b97f146e62a5ce60
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2024-04-28 15:08:50 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  plugins: [],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  purge: ["./index.html",'./src/**/*.{svelte,js,ts}'],
+  variants: {
+    extend: {},
+  },
+}
+
