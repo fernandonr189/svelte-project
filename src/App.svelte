ab97fae84eff2791f9899c2909d22412cdb2f82f
fernando <fernandonr189@outlook.com>
2024-04-28 18:29:50 -0600

    cambiado icono por google

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
   import PrimaryButton from './lib/PrimaryButton.svelte'
   import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+  import { faGoogle } from '@fortawesome/free-brands-svg-icons'
   
   let counter = 0
-  
+
   let increment = () => {
     counter += 1
   }
@@ -11,6 +12,6 @@
 
 <main>
   <div class="bg-slate-800 flex text-white h-screen p-5 justify-center items-center">
-    <PrimaryButton prefixIcon={faCartShopping} func={increment} title="Count is {counter}"/>
+    <PrimaryButton prefixIcon={faGoogle} func={increment} title="Count is {counter}"/>
   </div>
 </main>
