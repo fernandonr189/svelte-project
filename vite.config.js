3543edd0c31e0d53bed905b6138b9e8a6d91e0ab
Fernando Ismael Navarrete Ruiz <fernando.navarrete@toka.mx>
2024-07-03 16:57:13 -0600

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+    base: "/",
+    plugins: [svelte()],
+    preview: {
+        port: 8080,
+        strictPort: true,
+    },
+    server: {
+        port: 8080,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:8080",
+    },
 })
